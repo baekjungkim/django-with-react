4e8b5199c5590444e481c8a97ba6664d6d4acc51
baekjung <zvgandam@gmail.com>
2020-06-30 17:19:27 +0900

    04 - 장고가 media파일을 다루는 방법

--- instagram/admin.py
+++ instagram/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
 from .models import Post
 
 # 등록방법 1
@@ -16,6 +18,7 @@ from .models import Post
 class PostAdmin(admin.ModelAdmin):
     list_display = [
         "id",
+        "photo_tag",
         "message",
         "message_length",
         "is_public",
@@ -26,8 +29,12 @@ class PostAdmin(admin.ModelAdmin):
     list_filter = ["created_at", "is_public"]
     search_fields = ["message"]
 
+    def photo_tag(self, post):
+        if post.photo:
+            return mark_safe(f"<img src='{post.photo.url}' style='width: 72px;' />")
+        return None
+
     def message_length(self, post):
         return f"{len(post.message)} 글자"
 
     message_length.short_description = "메세지 글자수"
-
