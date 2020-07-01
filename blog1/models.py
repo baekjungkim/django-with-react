16b7c43644a80a4c676254cdac131f8d32f77d79
baekjung <zvgandam@gmail.com>
2020-07-01 17:06:07 +0900

    장고 Models) 14 - 마이그레이션을 통한 데이터베이스 스키마 관리 (2)

--- blog1/models.py
+++ blog1/models.py
@@ -1,7 +1,13 @@
+from django.conf import settings
 from django.db import models
 
 
 class Post(models.Model):
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="+",  # instagram app 에 같은 모델이 있기때문에 name 충돌 에러 발생해서 포기시킴
+    )
     title = models.CharField(max_length=100)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
