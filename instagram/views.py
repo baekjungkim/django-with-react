2a5b96a40fd8891f364ec67eed81322e2c91514e
baekjung <zvgandam@gmail.com>
2020-07-01 17:42:14 +0900

    장고 Views) 01 - 다양한 응답의 함수 기반 뷰 (1)

--- instagram/views.py
+++ instagram/views.py
@@ -1,10 +1,16 @@
+from django.views.generic import ListView
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from .models import Post
 
-# reqeust.GET
-# request.POST
-# request.FILES
+# Class Based View
+# post_list = ListView.as_view(model=Post)
+
+# Function Based View
 def post_list(request):
+    # reqeust.GET
+    # request.POST
+    # request.FILES
     qs = Post.objects.all()
     q = request.GET.get("query", "")
 
@@ -15,3 +21,8 @@ def post_list(request):
 
     # instagram/templates/instagram/post_list.html
     return render(request, "instagram/post_list.html", {"post_list": qs, "q": q})
+
+
+def post_detail(request: HttpRequest, pk: int) -> HttpResponse:
+
+    return render(request, "instagram/post_list.html", {})
