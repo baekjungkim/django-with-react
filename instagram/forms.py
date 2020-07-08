a24d12842e511eef403f7211588776f1257d8413
baekjung <zvgandam@gmail.com>
2020-07-08 12:13:15 +0900

    장고 Forms) 06 - Form Validation

--- instagram/forms.py
+++ instagram/forms.py
@@ -1,3 +1,6 @@
+# python 정규 표현식
+# import re
+
 from django import forms
 from .models import Post
 
@@ -13,3 +16,11 @@ class PostForm(forms.ModelForm):
             "is_public",
         ]
         # exclude = [] # 특정 필드 배제 (사용하지 않기를 바람.)
+
+    # form.py 내의 clean_필드명 함수를 통한 유효성 검사 및 값 변경
+    # def clean_message(self):
+    #     message = self.cleaned_data.get("message")
+    #     if message:
+    #         message = re.sub(r"[a-zA-Z]+", "", message)
+    #     return message
+
