19c833541b0af5bf15d370445d8a3413fa933553
baekjung <zvgandam@gmail.com>
2020-07-13 14:52:55 +0900

    장고 기본 인증) 03. 템플릿 레이아웃에 링크 추가하기

--- accounts/urls.py
+++ accounts/urls.py
@@ -10,7 +10,9 @@ urlpatterns = [
         LoginView.as_view(template_name="accounts/login_form.html"),
         name="login",
     ),
-    path("logout/", LogoutView.as_view(), name="logout"),
+    # path("logout/", LogoutView.as_view(), name="logout"),
+    path("logout/", views.logout, name="logout"),
+    path("signup", views.signup, name="signup"),
     path("profile", views.profile, name="profile"),
     path("profile/edit", views.profile_edit, name="profile_edit"),
 ]
